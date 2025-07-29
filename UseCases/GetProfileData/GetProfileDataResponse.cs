b09aa046576d8491bd2a1fc3b28a0647ef395fa6
Thaís Michel <122589967+thaiiss7@users.noreply.github.com>
2025-07-29 10:53:04 -0300

    update getprofiledata response

--- UseCases/GetProfileData/GetProfileDataResponse.cs
+++ UseCases/GetProfileData/GetProfileDataResponse.cs
@@ -1,3 +1,14 @@
 namespace Pinterest.UseCases.GetProfileData;
 
-public record GetProfileDataResponse;
+public record GetProfileDataFolder(
+    string Title,
+    string Picture,
+    // int NumberOfPins
+    // vou ver como implemento depois
+);
+
+public record GetProfileDataResponse(
+    string Username,
+    string Bio,
+    ICollection<GetProfileDataFolder> Folders
+);
