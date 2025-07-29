6bede365740d22db7d3ba1aac9a90a55630447a8
Thaís Michel <122589967+thaiiss7@users.noreply.github.com>
2025-07-29 10:32:34 -0300

    comment createprofile payload

--- UseCases/CreateProfile/CreateProfilePayload.cs
+++ UseCases/CreateProfile/CreateProfilePayload.cs
@@ -2,5 +2,6 @@ namespace Pinterest.UseCases.CreateProfile;
 
 public record CreateProfilePayload
 {
-    
+    // informações passadas na hora que cria uma conta
+    // criar pasta Validantions para validações específicas
 }
