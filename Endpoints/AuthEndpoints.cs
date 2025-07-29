ac1225cf40eb04ccf1e212b51b9cef17e6877ff7
Thaís Michel <122589967+thaiiss7@users.noreply.github.com>
2025-07-29 10:23:50 -0300

    add auth endpoints

---
+++ Endpoints/AuthEndpoints.cs
@@ -0,0 +1,21 @@
+using Pinterest.UseCases.Login;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pinterest.Endpoints;
+
+public static class AuthEndpoints
+{
+    public static void ConfigureAuthEndpoints(this WebApplication app)
+    {
+        app.MapPost("auth", async (
+            [FromBody]LoginPayload payload,
+            [FromServices]LoginUseCase useCase) =>
+        {
+            var result = await useCase.Do(payload);
+            if (!result.IsSuccess)
+                return Results.BadRequest();
+            
+            return Results.Ok(result.Data);
+        });
+    }
+}
