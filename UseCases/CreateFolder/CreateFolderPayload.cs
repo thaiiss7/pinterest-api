cfd792c4ebdf1051ec67986ea55e66f28624b3c0
Thaís Michel <122589967+thaiiss7@users.noreply.github.com>
2025-07-29 10:30:38 -0300

    comment createfolder payload

--- UseCases/CreateFolder/CreateFolderPayload.cs
+++ UseCases/CreateFolder/CreateFolderPayload.cs
@@ -2,5 +2,6 @@ namespace Pinterest.UseCases.CreateFolder;
 
 public record CreateFolderPayload
 {
-    
+    // informações passadas na hora que cria uma conta
+    // criar pasta Validantions para validações específicas
 }
