afecf292cd2e65307c66e35a0cc1656d5f3c6c70
Thaís Michel <122589967+thaiiss7@users.noreply.github.com>
2025-07-29 16:59:06 -0300

    folder endpoints meio zoado

--- Endpoints/FolderEndpoints.cs
+++ Endpoints/FolderEndpoints.cs
@@ -1,2 +1,20 @@
-// MapGet para buscar a pasta por id
-// MapPost para criar uma pasta
+namespace Pinterest.Endpoints;
+
+public static class FolderEndpoints
+{
+    public static void ConfigureFolderEndpoints(this WebApplication app)
+    {
+        // MapPost para criar uma pasta
+        app.MapPost("folder", async (
+            [FromBody]CreateFolderPayload payload,
+            [FromServices]CreateFolderUseCase useCase) => 
+            {
+                var result = await useCase.Do(payload);
+                if (result.IsSuccess)
+                
+            });
+
+        // MapGet para buscar a pasta por id
+
+    }
+}
