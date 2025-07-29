d2480196ecf208e4e83201cc481481e6bf05f3d9
Thaís Michel <122589967+thaiiss7@users.noreply.github.com>
2025-07-29 10:05:34 -0300

    update pin endpoints comments

--- Endpoints/PinEndpoints.cs
+++ Endpoints/PinEndpoints.cs
@@ -1,3 +1,4 @@
-//MapGet para buscar um pin pelo id
-//MapPost para publicar um pin
-//MapDelete para deletar um pin por id
+// MapGet para buscar um pin pelo id
+// MapPost para publicar um pin
+// MapDelete para deletar um pin por id
+// MapPost para salvar um pin em uma pasta 
