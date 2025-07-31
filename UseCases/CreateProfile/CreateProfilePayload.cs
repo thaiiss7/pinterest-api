79b0d9f493e3b98a3944464dcace8de21cc6211d
thaiiss7 <thaismichel7@gmail.com>
2025-07-30 21:25:25 -0300

    add validations

--- UseCases/CreateProfile/CreateProfilePayload.cs
+++ UseCases/CreateProfile/CreateProfilePayload.cs
@@ -1,7 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using Pinterest.Validations;
+
 namespace Pinterest.UseCases.CreateProfile;
 
 public record CreateProfilePayload
 {
     // informações passadas na hora que cria uma conta
-    // criar pasta Validantions para validações específicas
+
+    [Required]
+    [MinLength(5)]
+    [MaxLength(20)]
+    public string Username { get; init; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; }
+
+    [Required]
+    [MinLength(5)]
+    [NeedNumber]
+    public string Password { get; init; }
+
+    [Required]
+    [Compare("Password")]
+    public string RepeatPassword { get; init; }
+
+    [MaxLength(200)]
+    public string ?Bio { get; init; }
 }
--- UseCases/CreateProfile/CreateProfileUseCase.cs
+++ UseCases/CreateProfile/CreateProfileUseCase.cs
@@ -1,6 +1,24 @@
+using Pinterest.Models;
+
 namespace Pinterest.UseCases.CreateProfile;
 
 public class CreateProfileUseCase
 {
-    
+    //IProfilesService profilesService,
+    //IPasswordService passwordService
+}
+{
+    // public async Task<Result<CreateProfileResponse>> Do(CreateProfilePayload payload)
+    // {
+    //     var profile = new Profile {
+    //         Bio = payload.Bio,
+    //         Email = payload.Email,
+    //         Username = payload.Username,
+    //         Password = passwordService.Hash(payload.Password)
+    //     };
+
+    //     await profilesService.Create(profile);
+
+    //     return Result<CreateProfileResponse>.Success(new());
+    // }
 }
