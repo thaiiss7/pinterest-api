39582a2564a1585bd24f861a63a27d43f12a80f9
thaiiss7 <thaismichel7@gmail.com>
2025-08-05 20:27:30 -0300

    update program

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Pinterest.Endpoints;
+using Pinterest.EndPoints;
 using Pinterest.Models;
+using Pinterest.Services.JWT;
+using Pinterest.Services.Password;
+using Pinterest.Services.Profiles;
+using Pinterest.UseCases.CreateFolder;
+using Pinterest.UseCases.CreateProfile;
+using Pinterest.UseCases.DeletePin;
+using Pinterest.UseCases.DeleteProfile;
+using Pinterest.UseCases.GetFolderData;
+using Pinterest.UseCases.GetPinData;
+using Pinterest.UseCases.GetProfileData;
+using Pinterest.UseCases.Login;
+using Pinterest.UseCases.PostPin;
+using Pinterest.UseCases.SavePin;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +27,7 @@ builder.Services.AddDbContext<PinterestDbContext>(options => {
 });
 
 builder.Services.AddTransient<IPasswordService, PBKDF2PasswordService>();
-builder.Services.AddTransient<IProfilesService, EFProfileService>();
+builder.Services.AddTransient<IProfileService, EFProfileService>();
 builder.Services.AddSingleton<IJWTService, JWTService>();
 
 builder.Services.AddTransient<CreateFolderUseCase>();
@@ -21,6 +39,7 @@ builder.Services.AddTransient<GetProfileDataUseCase>();
 builder.Services.AddTransient<GetPinDataUseCase>();
 builder.Services.AddTransient<GetFolderDataUseCase>();
 builder.Services.AddTransient<SavePinUseCase>();
+builder.Services.AddTransient<DeleteProfileUseCase>();
 
 var jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET");
 var keyBytes = Encoding.UTF8.GetBytes(jwtSecret);
@@ -41,7 +60,13 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.ConfigureAuthEndpoints();
 app.ConfigureFolderEndpoints();
